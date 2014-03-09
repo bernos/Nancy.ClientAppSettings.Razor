ac9fe76cddb1b2eb23a3947e4292ee28e74bbe12
Brendan McMahon <bernos@gmail.com>
2014-03-09 14:23:34 +1100

    Added html view helpers

---
+++ Nancy.ClientAppSettings.Razor/HtmlHelperExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nancy.ViewEngines.Razor;
+using Nancy.ClientAppSettings;
+
+namespace Nancy.ClientAppSettings.Razor
+{
+    public static class HtmlHelperExtensions
+    {
+        /// <summary>
+        /// Retrieves the ClientAppSettings object from the current NancyContext
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="helpers"></param>
+        /// <returns></returns>
+        public static ClientAppSettings ClientAppSettings<T>(this HtmlHelpers<T> helpers)
+        {
+            return helpers.RenderContext.Context.GetClientAppSettings();
+        }
+
+        /// <summary>
+        /// Renders the client app settings javascript.
+        /// </summary>
+        /// <param name="includeScriptElement">Determines whether to render the app settings wrapped in an html script element or not. Defaults to True</param>
+        /// <returns></returns>
+        public static IHtmlString RenderClientAppSettings<T>(this HtmlHelpers<T> helpers, bool includeScriptElement = true)
+        {
+            var settings = helpers.RenderContext.Context.GetClientAppSettings();
+            var js = string.Format("var {0} = {1};", settings.VariableName, settings.ToJson(helpers.RenderContext.Context));
+
+            if (includeScriptElement)
+            {
+                js = string.Format("<script>{0}</script>", js);
+            }
+
+            return new NonEncodedHtmlString(js);
+        }
+    }
+}
--- Nancy.ClientAppSettings.Razor/Properties/AssemblyInfo.cs
+++ Nancy.ClientAppSettings.Razor/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Nancy.ClientAppSettings.Razor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("HtmlHelper extensions for the Razor view engine to make it easy to output client app settings")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Brendan McMahon")]
 [assembly: AssemblyProduct("Nancy.ClientAppSettings.Razor")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.*")]
+[assembly: AssemblyFileVersion("0.0.1.*")]
